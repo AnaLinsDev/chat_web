48623ef0b23078f43751b9429d8ec5a22cb8c910
AnaLinsDev <analinsdev@gmail.com>
2023-04-24 17:14:26 -0300

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app dark>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "DefaultLayout",
+};
+</script>
+
+<style scoped>
+main {
+  background-image: linear-gradient(
+    to right,
+    rgba(6, 227, 76, 0.407),
+    rgba(0, 64, 255, 0.374)
+  );
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12">
+      <v-card
+        class="logo py-4 d-flex justify-center"
+        height="calc(100vh - 30px)"
+        elevation="0"
+      >
+        chat here
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
